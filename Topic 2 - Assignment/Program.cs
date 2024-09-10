a274be0508e253138064b6e3f3e898da530b9567
Emmett Cornies <339761330@GoTVDSB.ca>
2024-09-10 09:38:04 -0400

    Add project files.

---
+++ Topic 2 - Assignment/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Topic_2___Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   // Emmett Cornies
+            Console.WriteLine(2024 - 16);
+            Console.WriteLine((14 + 15 + 16) / 3);
+            Console.WriteLine(Math.Round((14.0 + 15 + 16 + 17) / 4, 2));
+            Console.WriteLine(Math.Round(Math.PI * (3 * 3), 1) + "cm^2");
+            Console.WriteLine("C# looks at both values on each side of the plus sign, and if either both of them are strings or one string and one integer, it will");
+        }
+    }
+}
